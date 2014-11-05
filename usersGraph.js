3615eb4ee45fe7e408005e2da2a7f39ea30e3f95
evan1590 <evanc1590@aol.com>
2014-11-05 12:25:26 -0500

    Fixed the height of the graphs
    the graph heights were expanding outside the div but fixed it by
    dividing the height by 100 or 10

--- usersGraph.js
+++ usersGraph.js
@@ -1,36 +1,35 @@
 var svgWidth = 250;
-var svgHeight = 250;
-var barPadding = 1; 
+var svgHeight = 300;
+var barPadding = 10; 
 
-// Creat SVG element
-var svg = d3.select("#users")
-			.append("svg")
-			.attr("width", svgWidth)
-			.attr("height", svgHeight);
-
-var dataset = [];
-var month = [];
-// doing this asynchronously arrrgggggggggggggg! 
 $.getJSON('https://spreadsheets.google.com/feeds/list/1gs8MWVzYQQ90xYe04JGitxE_5wwOQCHlNe4706vVS34/od6/public/values?alt=json', function(data) { 
-		console.log(data);
+		//console.log(data);
+		var dataset = [];
+		var month = [];
 	 	lengthofArray = data.feed.entry.length;
 	 	for (i = 0; i < lengthofArray; i++) { 
 			//console.log(data.feed.entry[i]['gsx$state']['$t']);
 			//console.log(data.feed.entry[i]['gsx$numberofusers']['$t']);
 			month.push(data.feed.entry[i]['gsx$users']['$t']);
-			//console.log(month)
+			
 		};
-	console.log(month);
+	
 	dataset.push(month[9]);
 	dataset.push(month[21]);
 	console.log(dataset);
+	
+	// Creat SVG element
+	var svg = d3.select("#users")
+			.append("svg")
+			.attr("width", svgWidth)
+			.attr("height", svgHeight);
 
 	svg.selectAll ("rect")
 		.data(dataset)
 		.enter()
 		.append("rect")
 		.attr("x", function(d, i) {
-			return i * (svgWidth / dataset.length); //Bar width of 20 plus 1 for padding
+			return i * (svgWidth / dataset.length); //Bar width of 20 plus 10 for padding
 		})
 		.attr("y", function(d) {
 			return svgHeight - d;  //Height minus data value
@@ -49,7 +48,7 @@ $.getJSON('https://spreadsheets.google.com/feeds/list/1gs8MWVzYQQ90xYe04JGitxE_5
 			return d;
 			})
 	   	.attr("x", function(d, i) {
-			return i * (svgWidth / dataset.length) + 30;
+			return i * (svgWidth / dataset.length) + 40;
 		})
 			.attr("y", function(d) {
 				 return svgHeight - d + 14;
@@ -59,4 +58,133 @@ $.getJSON('https://spreadsheets.google.com/feeds/list/1gs8MWVzYQQ90xYe04JGitxE_5
 			.attr("fill", "white")
 			.attr("text-anchor", "middle");
 
+	$.getJSON('https://spreadsheets.google.com/feeds/list/1gs8MWVzYQQ90xYe04JGitxE_5wwOQCHlNe4706vVS34/od6/public/values?alt=json', function(data) { 
+			//console.log(data);
+			var dataset = [];
+			var month = [];
+			var textValue = [];
+			var tobeDisplayed = [];
+		 	lengthofArray = data.feed.entry.length;
+		 	for (i = 0; i < lengthofArray; i++) { 
+				//console.log(data.feed.entry[i]['gsx$state']['$t']);
+				//console.log(data.feed.entry[i]['gsx$numberofusers']['$t']);
+				textValue.push(data.feed.entry[i]['gsx$sesions']['$t']);
+				var value = data.feed.entry[i]['gsx$sesions']['$t'];
+				var result = value / 10;
+				month.push(result);
+				
+			};
+		
+		dataset.push(month[9]);
+		dataset.push(month[21]);
+		tobeDisplayed.push(textValue[9]);
+		tobeDisplayed.push(textValue[21]);
+		console.log(dataset);
+		
+		// Creat SVG element
+		var svg = d3.select("#sessions")
+				.append("svg")
+				.attr("width", svgWidth)
+				.attr("height", svgHeight);
+
+		svg.selectAll ("rect")
+			.data(dataset)
+			.enter()
+			.append("rect")
+			.attr("x", function(d, i) {
+				return i * (svgWidth / dataset.length); //Bar width of 20 plus 1 for padding
+			})
+			.attr("y", function(d) {
+				return svgHeight - d;  //Height minus data value
+			})
+			.attr("width", svgWidth / dataset.length - barPadding)
+			.attr("height", function(d) {
+				return d; 
+			})
+			.attr("fill", "purple");
+
+		svg.selectAll("text")
+		   	.data(tobeDisplayed)
+		   	.enter()
+		   	.append("text")
+		   	.text(function(d) {
+				return d;
+				})
+		   	.attr("x", function(d, i) {
+				return i * (svgWidth / dataset.length) + 45;
+			})
+				.attr("y", function(d) {
+					 return svgHeight - (d / 10) + 14;
+				})
+				.attr("font-family", "sans-serif")
+				.attr("font-size", "11px")
+				.attr("fill", "white")
+				.attr("text-anchor", "middle");
+		$.getJSON('https://spreadsheets.google.com/feeds/list/1gs8MWVzYQQ90xYe04JGitxE_5wwOQCHlNe4706vVS34/od6/public/values?alt=json', function(data) { 
+					//console.log(data);
+					var dataset = [];
+					var month = [];
+					var textValue = [];
+					var tobeDisplayed = [];
+				 	lengthofArray = data.feed.entry.length;
+				 	for (i = 0; i < lengthofArray; i++) { 
+						//console.log(data.feed.entry[i]['gsx$state']['$t']);
+						//console.log(data.feed.entry[i]['gsx$numberofusers']['$t']);
+						textValue.push(data.feed.entry[i]['gsx$pageviews']['$t']);
+						var value = data.feed.entry[i]['gsx$pageviews']['$t'];
+						var result = value / 100;
+						month.push(result);
+						
+					};
+				
+				dataset.push(month[9]);
+				dataset.push(month[21]);
+				tobeDisplayed.push(textValue[9]);
+				tobeDisplayed.push(textValue[21]);
+				console.log(dataset);
+				
+				// Creat SVG element
+				var svg = d3.select("#pageviews")
+						.append("svg")
+						.attr("width", svgWidth)
+						.attr("height", svgHeight);
+
+				svg.selectAll ("rect")
+					.data(dataset)
+					.enter()
+					.append("rect")
+					.attr("x", function(d, i) {
+						return i * (svgWidth / dataset.length); //Bar width of 20 plus 1 for padding
+					})
+					.attr("y", function(d) {
+						return svgHeight - d;  //Height minus data value
+					})
+					.attr("width", svgWidth / dataset.length - barPadding)
+					.attr("height", function(d) {
+						return d; 
+					})
+					.attr("fill", "purple");
+
+				svg.selectAll("text")
+				   	.data(tobeDisplayed)
+				   	.enter()
+				   	.append("text")
+				   	.text(function(d) {
+						return d;
+						})
+				   	.attr("x", function(d, i) {
+						return i * (svgWidth / dataset.length) + 45;
+					})
+						.attr("y", function(d) {
+							 return svgHeight - (d / 100) + 14;
+						})
+						.attr("font-family", "sans-serif")
+						.attr("font-size", "11px")
+						.attr("fill", "white")
+						.attr("text-anchor", "middle");
+				
+				
+			});
+		
 	});
+});
