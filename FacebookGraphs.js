391ff9d118714bb9e72c53885f4be49db1c932ef
Evan Chessman <chessman_evan@wheatoncollege.edu>
2014-11-17 10:35:27 -0500

    working on facebook post

--- FacebookGraphs.js
+++ FacebookGraphs.js
@@ -135,9 +135,12 @@ $.getJSON('https://spreadsheets.google.com/feeds/list/1gs8MWVzYQQ90xYe04JGitxE_5
 				var dataset = [];
 				dataset.push(data.feed.entry[21]['gsx$mostpopularfacebookpost']['$t']);
 				$('#mostPopPost').append(dataset[0]);
+				//$('#mostPopPost').append("<div id='fb-root'></div><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = '//connect.facebook.net/en_US/all.js#xfbml=1'; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk')); </script> <div class='fb-post' data-href=" + dataset[0] + " 'https://www.facebook.com/WheatonCollege/posts/10152813567153328'></div>");
 					
 			});
 			
 		});
 	
 });
+
+
